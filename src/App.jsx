8dc832d281342c10d602bd2b0ab66ea3c3cd177d
steven-frasica <steven.frasica@gmail.com>
2026-04-25 19:49:40 -0400

    install tailwind and start sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Sidebar from './components/Sidebar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='h-screen bg-black'>
+      <div className="h-[90%] flex"></div>
+      <Sidebar />
+    </div>
   )
 }
 
