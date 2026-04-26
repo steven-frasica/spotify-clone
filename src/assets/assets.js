d0edf17f66b297d8e49707d3a840471fef7b9381
steven-frasica <steven.frasica@gmail.com>
2026-04-26 11:20:45 -0400

    complete album item component

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -79,14 +79,14 @@ export const albumsData = [
     },
     {   
         id:1,
-        name: "Top 50 India",
+        name: "Top 50 USA",
         image: img9,
         desc:"Your weekly update of the most played tracks",
         bgColor:"#22543d"
     },
     {   
         id:2,
-        name: "Trending India",
+        name: "Trending USA",
         image: img10,
         desc:"Your weekly update of the most played tracks",
         bgColor:"#742a2a"
