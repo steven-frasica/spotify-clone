1ad7ad8b07936dde0c9875a40e2df5f38cd3c0c9
steven-frasica <steven.frasica@gmail.com>
2026-04-30 11:35:04 -0400

    fix for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   plugins: [react(), tailwindcss()],
 })
