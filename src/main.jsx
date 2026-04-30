2f30076b917af593ad7ae56561d102e9727c1bd9
steven-frasica <steven.frasica@gmail.com>
2026-04-30 11:29:17 -0400

    edit path

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import PlayerContextProvider from "./context/PlayerContext";
 
 createRoot(document.getElementById("root")).render(
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/spotify-clone/',
+  base: './',
   plugins: [react(), tailwindcss()],
 })
